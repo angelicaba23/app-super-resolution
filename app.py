931464bcd8e63bc194ba1899a06a1bd054bd5ada
Angelica Barranco <angelicaba9923@gmail.com>
2022-03-23 18:03:51 -0500

    crop tool added

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 #!pip install git+https://github.com/hukkelas/DSFD-Pytorch-Inference.git
 from io import StringIO
 import cv2
-from face_dectec import faceDetection
+from face_dectec import crop_object, faceDetection
+from streamlit_cropper import st_cropper
 import streamlit as st
 import numpy as np
-import os
+
 from PIL import Image
+
+from save_img import save_image
 
 image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 if image_file is not None:
-  file_details = {"FileName": image_file.name,
-                  "FileType": image_file.type}
-  st.write(file_details)
+  save_image(image_file, image_file.name)
 
-  # @ save image
-  with open(os.path.join("uploaded_image", image_file.name), "wb") as f:
-    f.write(image_file.getbuffer())
-
-  img_file = "uploaded_image/" + file_details['FileName']
-
-  img_faces = faceDetection(img_file)
+  img_file = "uploaded_image/" + image_file.name
+  
+  [img_faces, num, names] = faceDetection(img_file)
+  st.write(num)
+  st.write(names)
   st.image(img_faces)
-
+  
+  aspect_dict = {
+      "1:1": (1, 1),
+      "16:9": (16, 9),
+      "4:3": (4, 3),
+      "2:3": (2, 3),
+      "Free": None
+  }
+  aspect_ratio = aspect_dict["Free"]
+  if img_file:
+    img = Image.open(img_file)
+    #if not realtime_update:
+    #    st.write("Double click to save crop")
+    # Get a cropped image from the frontend
+    
+    cropped_img = st_cropper(img, realtime_update=True, box_color='#0000FF')
+    if st.button('CROP'):
+      st.image(crop_object(img, cropped_img, num))
+    # Manipulate cropped image at will
+    st.write("Preview")
+    
+    _ = cropped_img.thumbnail((150,150))
+    st.image(cropped_img)
--- face_dectec.py
+++ face_dectec.py
@@ -1,23 +1,46 @@
 import cv2
 import face_detection
+import streamlit as st
+from PIL import Image, ImageDraw
+from PIL import ImageOps
+
 print(face_detection.available_detectors)
 detector = face_detection.build_detector(
   "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
 
+def crop_object(image, box, num, names):
+  x_top_left = box[0]
+  y_top_left = box[1]
+  x_bottom_right = box[2]
+  y_bottom_right = box[3]
 
+  crop_img = image.crop((int(x_top_left), int(y_top_left), int(x_bottom_right), int(y_bottom_right)))
+  name = "crop_img_"+str(num)+".png"
+  names.append(name)
+  crop_img.save(name)
+  return crop_img
+
+@st.cache(suppress_st_warning=True)
 def faceDetection(input_image_path):
   im = cv2.imread(input_image_path)[:, :, ::-1]
   detections = detector.detect(im)
   print(len(detections))
   num=0
 
-  image = cv2.imread(input_image_path)
+  image_landmarks = cv2.imread(input_image_path)
+  names = [] 
+  img_cancel  = Image.open("cancelar.png")
   for detections in detections:
     x = int(detections[0])
     y = int(detections[1])
     w = int(detections[2])
     h = int(detections[3])
-    cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
+    cv2.rectangle(image_landmarks, (x, y), (w, h), (0, 255, 0), 2)
+    print(x, y, w, h)
+    cv2.putText(image_landmarks, 'X', (w-6, y+6),cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0,255), 3,cv2.LINE_AA )
+    image = Image.open(input_image_path)
+    st.image(crop_object(image, detections, num, names))
+
     num+=1
-  image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  return image
+  image_landmarks = cv2.cvtColor(image_landmarks, cv2.COLOR_BGR2RGB)
+  return image_landmarks, num, names
