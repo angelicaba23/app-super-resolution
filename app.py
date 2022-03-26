3b21c5fb34c629bdbc401b7d1d7bd7c73f8af65b
Angelica Barranco <angelica.barranco@ibm.com>
2022-03-26 11:41:00 -0500

    canvas

--- app.py
+++ app.py
@@ -21,30 +21,5 @@ if image_file is not None:
   st.image(img_faces)
   st.image(names[0])
 
-# Specify canvas parameters in application
-drawing_mode = "rect"
-stroke_width = 3
-realtime_update = True
-stroke_color = "#000000"
-bg_color = "#eee"
-
-# Create a canvas component
-if img_file is not None:
-  bg_image = Image.open(img_file)
-  label_color = (
-      st.sidebar.color_picker("Annotation color: ", "#EA1010") + "77"
-  )  # for alpha from 00 to FF
-  label = st.sidebar.text_input("Label", "Default")
-  mode = "transform" if st.sidebar.checkbox("Move ROIs", False) else "rect"
-
-  canvas_result = st_canvas(
-      fill_color=label_color,
-      stroke_width=3,
-      background_image=bg_image,
-      height=320,
-      width=512,
-      drawing_mode=mode,
-      key="color_annotation_app",
-  )
 
 
