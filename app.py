49370200fe570ca91957e32dba0c6dd7a3b066ae
Angelica Barranco <angelica.barranco@ibm.com>
2022-03-26 11:13:12 -0500

    canvas

--- app.py
+++ app.py
@@ -29,18 +29,19 @@ stroke_color = "#000000"
 bg_color = "#eee"
 
 # Create a canvas component
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=Image.open(image_file) if image_file else None,
-    update_streamlit=realtime_update,
-    height=150,
-    drawing_mode=drawing_mode,
-    #point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-    key="canvas",
-)
+if image_file is not None:
+  canvas_result = st_canvas(
+      fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+      stroke_width=stroke_width,
+      stroke_color=stroke_color,
+      background_color=bg_color,
+      background_image=Image.open(image_file) if image_file else None,
+      update_streamlit=realtime_update,
+      height=150,
+      drawing_mode=drawing_mode,
+      #point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+      key="canvas",
+  )
 
 # Do something interesting with the image data and paths
 if canvas_result.image_data is not None:
