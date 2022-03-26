faa5dc7afe61fad8bd5f49cd4cf700fa471b7c94
Angelica Barranco <angelica.barranco@ibm.com>
2022-03-26 15:31:34 -0500

    canvas

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ import pandas as pd
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
+im = Image.open("icon.ico")
+st.set_page_config(
+    page_title="SuperResolution",
+    page_icon=im,
+    layout="wide",
+)
+
 image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 if image_file is not None:
   save_image(image_file, image_file.name)
