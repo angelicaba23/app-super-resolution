98247229cfec9cd5b0255ea027e047ecf6ed3df3
Angelica Barranco <angelica.barranco@ibm.com>
2022-03-26 22:26:23 -0500

    overwrite json

--- write_json.py
+++ write_json.py
@@ -2,7 +2,7 @@ import json
 
 # function to add to JSON
 
-def write_json(new_data, filename='save_state.json', group="objects"):
+def write_json(new_data, filename='saved_state.json', group="objects"):
     with open(filename,'r+') as file:
           # First we load existing data into a dict.
         file_data = json.load(file)
