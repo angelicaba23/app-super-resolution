cd9c4ba8cd87a44aee0f82f486aafc39cab3209b
Angelica Barranco <angelica.barranco@ibm.com>
2022-04-22 17:16:02 -0500

    test img

--- app.py
+++ app.py
@@ -23,86 +23,86 @@ st.set_page_config(
 image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 if image_file is not None:
   save_image(image_file, image_file.name)
-
   img_file = "uploaded_image/" + image_file.name
   
 else:
   if st.button("TEST") is None:
     img_file = "selfie3.jpeg"
-  
-[img_faces, num, boxes] = faceDetection(img_file)
-#st.write(boxes)
-#st.image(img_faces)
-if len(boxes) > 0:
-  list = []
-  filename = 'saved_state.json'
 
-  for boxes in boxes:
-    list.append({
-      "type": "rect",
-        "left": boxes[0],
-        "top": boxes[1],
-        "width": boxes[2]-boxes[0],
-        "height": boxes[3]-boxes[1],
-        "fill": "#00ff0050",
-        "stroke": "#00ff00",
-        "strokeWidth": 3
-    })
+if img_file is not None:  
+  [img_faces, num, boxes] = faceDetection(img_file)
+  #st.write(boxes)
+  #st.image(img_faces)
+  if len(boxes) > 0:
+    list = []
+    filename = 'saved_state.json'
 
-  # Verify updated list
-  #st.write(list)
+    for boxes in boxes:
+      list.append({
+        "type": "rect",
+          "left": boxes[0],
+          "top": boxes[1],
+          "width": boxes[2]-boxes[0],
+          "height": boxes[3]-boxes[1],
+          "fill": "#00ff0050",
+          "stroke": "#00ff00",
+          "strokeWidth": 3
+      })
 
-  listObj = {
-      "version": "4.4.0",
-      "objects": list  
-  }
+    # Verify updated list
+    #st.write(list)
 
-  # Verify updated listObj
-  #st.write(listObj)
+    listObj = {
+        "version": "4.4.0",
+        "objects": list  
+    }
 
-  with open(filename, 'w') as json_file:
-    json.dump(listObj, json_file, 
-                        indent=4,  
-                        separators=(',',': '))
+    # Verify updated listObj
+    #st.write(listObj)
 
-  with open(filename, "r") as f:   saved_state = json.load(f)
-  #st.write(saved_state)
-  
-  bg_image = Image.open(img_file)
-  label_color = (
-      st.sidebar.color_picker("Annotation color: ", "#00ff00") + "50"
-  )  # for alpha from 00 to FF
-  tool_mode = st.sidebar.selectbox(
-    "Select tool:", ("draw", "move")
-  )
-  mode = "transform" if tool_mode=="move" else "rect"
-  
-  with st.spinner('Wait for it...'):
-    canvas_result = st_canvas(
-      fill_color=label_color,
-      stroke_width=3,
-      stroke_color="#00ff00",
-      background_image=bg_image,
-      height=bg_image.height,
-      width=bg_image.width,
-      initial_drawing=saved_state,
-      drawing_mode=mode,
-      key="color_annotation_app",
-  )
+    with open(filename, 'w') as json_file:
+      json.dump(listObj, json_file, 
+                          indent=4,  
+                          separators=(',',': '))
 
-  if canvas_result.json_data is not None:
-      
-      rst_objects = canvas_result.json_data["objects"]
-      objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
+    with open(filename, "r") as f:   saved_state = json.load(f)
+    #st.write(saved_state)
+    
+    bg_image = Image.open(img_file)
+    label_color = (
+        st.sidebar.color_picker("Annotation color: ", "#00ff00") + "50"
+    )  # for alpha from 00 to FF
+    tool_mode = st.sidebar.selectbox(
+      "Select tool:", ("draw", "move")
+    )
+    mode = "transform" if tool_mode=="move" else "rect"
+    
+    with st.spinner('Wait for it...'):
+      canvas_result = st_canvas(
+        fill_color=label_color,
+        stroke_width=3,
+        stroke_color="#00ff00",
+        background_image=bg_image,
+        height=bg_image.height,
+        width=bg_image.width,
+        initial_drawing=saved_state,
+        drawing_mode=mode,
+        key="color_annotation_app",
+    )
 
-      for rst_objects in rst_objects:
-        rts_boxes = [rst_objects['left'],rst_objects['top'],rst_objects['width']+rst_objects['left'],rst_objects['height']+rst_objects['top']]
-        #st.write(rts_boxes)
-        st.image(crop_object(bg_image, rts_boxes), use_column_width = "false")
+    if canvas_result.json_data is not None:
+        
+        rst_objects = canvas_result.json_data["objects"]
+        objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
 
-      #for col in objects.select_dtypes(include=['object']).columns:
-          #objects[col] = objects[col].astype("str")
-      #st.dataframe(objects)
-      
-else:
-  st.write("NO PERSON DETECTED")
+        for rst_objects in rst_objects:
+          rts_boxes = [rst_objects['left'],rst_objects['top'],rst_objects['width']+rst_objects['left'],rst_objects['height']+rst_objects['top']]
+          #st.write(rts_boxes)
+          st.image(crop_object(bg_image, rts_boxes), use_column_width = "false")
+
+        #for col in objects.select_dtypes(include=['object']).columns:
+            #objects[col] = objects[col].astype("str")
+        #st.dataframe(objects)
+        
+  else:
+    st.write("NO PERSON DETECTED")
