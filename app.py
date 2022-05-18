a52968bf89aac94b7450d0f7b7e9f1a78a406f3c
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-18 00:17:16 -0500

    test img

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-#!pip install git+https://github.com/hukkelas/DSFD-Pytorch-Inference.git
-
 import json
-import numpy as np
 import streamlit as st
 
 from face_dectec import crop_object, faceDetection
@@ -23,9 +20,8 @@ st.set_page_config(
 image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 if image_file is not None:
   save_image(image_file, image_file.name)
-  img_file = "uploaded_image/" + image_file.name
 
-  
+  img_file = "uploaded_image/" + image_file.name
   [img_faces, num, boxes] = faceDetection(img_file)
   #st.write(boxes)
   #st.image(img_faces)
