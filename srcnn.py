4520d58ca4de9ff1ece98da2803ebc1ecbd00174
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-18 08:08:04 -0500

    adding srcnn

--- srcnn.py
+++ srcnn.py
@@ -2,18 +2,16 @@ import sys
 import keras
 import cv2
 import numpy
-import matplotlib
 import skimage
 
 from keras.models import Sequential
 from keras.layers import Conv2D
-#from keras.optimizers import Adam
+from keras.optimizers import Adam
 
-from tensorflow.keras.optimizers import Adam
+#from tensorflow.keras.optimizers import Adam
 from skimage.metrics import structural_similarity as ssim
 
 #from skimage.measure import compare_ssim as ssim
-from matplotlib import pyplot as plt
 import numpy as np
 import math
 import os
@@ -23,4 +21,5 @@ from keras.models import Model
 from keras.models import Sequential
 
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from keras.applications import ImageDataGenerator, array_to_img, img_to_array
