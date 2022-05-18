393c6879921f08e6db34572cd953feda60037442
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-17 23:18:08 -0500

    test img

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if image_file is not None:
   save_image(image_file, image_file.name)
 
   img_file = "uploaded_image/" + image_file.name
-  [img_faces, num, boxes] = faceDetection(img_file)
+  [img_faces, num, boxes] = faceDetection(image_file)
   #st.write(boxes)
   #st.image(img_faces)
   if len(boxes) > 0:
@@ -61,7 +61,7 @@ if image_file is not None:
     with open(filename, "r") as f:   saved_state = json.load(f)
     #st.write(saved_state)
     
-    bg_image = Image.open(img_file)
+    bg_image = Image.open(image_file)
     label_color = (
         st.sidebar.color_picker("Annotation color: ", "#00ff00") + "50"
     )  # for alpha from 00 to FF
