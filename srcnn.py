9ee3ef22caa8fc86226dd4624b6e38761aa5e0cd
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-18 09:04:17 -0500

    adding srcnn

--- srcnn.py
+++ srcnn.py
@@ -13,8 +13,8 @@ from keras.optimizers import Adam
 from skimage.metrics import structural_similarity as ssim
 
 
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
-from keras.applications import ImageDataGenerator, array_to_img, img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+#from keras.applications import ImageDataGenerator, array_to_img, img_to_array
 
 
 # define the SRCNN model
