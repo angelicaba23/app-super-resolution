9ecab60823716c742d374b94a57abfff174130a7
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-17 23:46:43 -0500

    test img

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!pip install git+https://github.com/hukkelas/DSFD-Pytorch-Inference.git
+import io
 import json
 import cv2
 import numpy as np
@@ -26,11 +27,10 @@ if image_file is not None:
   img_file = "uploaded_image/" + image_file.name
 
   # Convert the file to an opencv image.
-  image = Image.open(image_file)
-  st.image(image, caption='Input', use_column_width=True)
-  img_array = np.array(image)
+  img_stream = io.BytesIO(image_file)
+  img = cv2.imdecode(np.frombuffer(img_stream.read(), np.uint8), 1)
 
-  [img_faces, num, boxes] = faceDetection(img_array)
+  [img_faces, num, boxes] = faceDetection(img)
   #st.write(boxes)
   #st.image(img_faces)
   if len(boxes) > 0:
