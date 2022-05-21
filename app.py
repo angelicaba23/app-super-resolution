7f544b351fe713e1b2ab988974b6f47f8bed9a87
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-21 15:12:58 -0500

    adding srgan

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from srcnn import predict
 import pandas as pd
 from streamlit_drawable_canvas import st_canvas
 
+st.set_page_config(
+    page_title="SuperResolution",
+    page_icon=Image.open("icon.ico"),
+    layout="wide"
+)
+
 image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
 if image_file is not None:
   #save_image(image_file, image_file.name)
