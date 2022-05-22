5a9dd914551fe45a81a2bc44635006261c0bc320
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-21 19:11:29 -0500

    adding srgan

--- srcnn.py
+++ srcnn.py
@@ -43,15 +43,17 @@ def model():
     # compile model
     SRCNN.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
     
+    load_weights = 'model_5545_993_20_10.h5'
+    #st.write(load_weights)
+    SRCNN.load_weights(load_weights)
+    print("weights loaded")
+    
     return SRCNN
 
 SRCNN = model()
 
 
-load_weights = 'model_5545_993_20_10.h5'
-#st.write(load_weights)
-SRCNN.load_weights(load_weights)
-print("weights loaded")
+
 
 def predict(input_img):
     scale = 2
