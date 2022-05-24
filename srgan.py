f450e7bf1b628b90cc7af0348ce6fa103342b43e
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-24 12:34:10 -0500

    adding srgan

--- srgan.py
+++ srgan.py
@@ -15,8 +15,8 @@ os.system("curl -LJO https://github.com/TencentARC/GFPGAN/releases/download/v0.1
 
 print(os.system("ls"))
 #print(os.system("ls pretrained_models "))
-os.system("BASICSR_EXT=True pip install basicsr==1.3.5")
-os.system("pip install realesrgan")
+#os.system("BASICSR_EXT=True pip install basicsr==1.3.5")
+os.system("curl https://files.pythonhosted.org/packages/5a/a9/7c9e5ff86c945d902aad5bcba0abf18c29c2bcd6ef0f078f414698d5c329/basicsr-1.3.5.tar.gzpip install realesrgan")
 print(os.system("pip list"))
 
 def predictSrgan():
@@ -28,3 +28,4 @@ restorer = GFPGANer(
         arch=arch,
         channel_multiplier=channel_multiplier,
         bg_upsampler=bg_upsampler)
+
