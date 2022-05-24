efc7f7962153405357a0db2ae034950015094cc9
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-24 01:32:36 -0500

    adding srgan

--- srgan.py
+++ srgan.py
@@ -1,4 +1,5 @@
 import os
+#from gfpgan import GFPGANer
 
 img_list = ""
 bg_upsampler = None
@@ -9,7 +10,17 @@ model_name = 'GFPGANv1.3'
 upscale = 2
 
 model_path = "GFPGAN/experiments/pretrained_models/GFPGANv1.pth"
-os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth -P pretrained_models")
+#os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth -P pretrained_models")
 
+print(os.system("ls"))
 def predictSrgan():
         print("GFPGANv1.pth descargado")
+
+"""
+restorer = GFPGANer(
+        model_path=model_path,
+        upscale=upscale,
+        arch=arch,
+        channel_multiplier=channel_multiplier,
+        bg_upsampler=bg_upsampler)
+ """       
