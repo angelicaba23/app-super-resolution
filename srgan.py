a762b71e49d4b38a1842a93da949ffc85f1c3c36
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-24 12:40:56 -0500

    adding srgan

--- srgan.py
+++ srgan.py
@@ -1,5 +1,6 @@
 import os
 from gfpgan import GFPGANer
+from BasicSR.basicsr import *
 
 img_list = ""
 bg_upsampler = None
@@ -10,17 +11,17 @@ model_name = 'GFPGANv1.3'
 upscale = 2
 
 model_path = "GFPGANv1.pth"
-print(os.system("pip list"))
 os.system("curl -LJO https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth")
 
-print(os.system("ls"))
 #print(os.system("ls pretrained_models "))
 #os.system("BASICSR_EXT=True pip install basicsr==1.3.5")
-os.system("curl https://files.pythonhosted.org/packages/5a/a9/7c9e5ff86c945d902aad5bcba0abf18c29c2bcd6ef0f078f414698d5c329/basicsr-1.3.5.tar.gzpip install realesrgan")
+#os.system("curl https://files.pythonhosted.org/packages/5a/a9/7c9e5ff86c945d902aad5bcba0abf18c29c2bcd6ef0f078f414698d5c329/basicsr-1.3.5.tar.gzpip install realesrgan")
 print(os.system("pip list"))
 
 def predictSrgan():
+        print(os.system('-d "GFPGANv1.pth'))
         print("GFPGANv1.pth descargado")
+        
 
 restorer = GFPGANer(
         model_path=model_path,
