d54eef4ff0448b3986b45ed368fd549c2bef56c4
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-24 13:05:52 -0500

    basicsr added

--- srgan.py
+++ srgan.py
@@ -14,8 +14,8 @@ model_path = "GFPGANv1.pth"
 os.system("curl -LJO https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth")
 
 #print(os.system("ls pretrained_models "))
-os.system("BASICSR_EXT=True pip install basicsr==1.3.5")
-os.system("python setup.py develop")
+#os.system("BASICSR_EXT=True pip install basicsr==1.3.5")
+#fromos.system("python setup.py develop")
 print(os.system("pip list"))
 
 def predictSrgan():
