6517634be4f0cf3bc5c2cc2a2948312090c7a7e5
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-25 08:45:35 -0500

    requirements updated

--- srgan.py
+++ srgan.py
@@ -107,6 +107,12 @@ def predictSrgan(image_path):
     if not os.path.isfile(model_path):
         raise ValueError(f'Model {model_name} does not exist.')
 
+    print('model_path='+model_path,
+        'upscale=='+args.upscale,
+        'arch='+arch,
+        'channel_multiplier='+channel_multiplier,
+        'bg_upsampler=='+bg_upsampler)
+        
     restorer = GFPGANer(
         model_path=model_path,
         upscale=args.upscale,
