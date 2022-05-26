5b66d0f26f205db3b76f40061840bd2466dc2a14
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-25 22:26:16 -0500

    extra added

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with coltry3:st.write(' ')
 
 with colbtt1:st.write(' ')
 with colbtt2:
-  if st.button("TRY IT"):
+  if st.checkbox("TRY IT"):
     image_file = 'extra/selfie3.jpeg'
     opencv_image= cv2.imread('extra/selfie3.jpeg')
 with colbtt3:st.write(' ')
