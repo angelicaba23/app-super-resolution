0e2917290336204dfbc2e3d8ff17c867a75d6701
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-25 22:19:46 -0500

    extra added

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ with colbtt1:st.write(' ')
 with colbtt2:
   if st.button("TRY IT"):
     image_file = 'extra/selfie3.jpeg'
+    opencv_image= cv2.imread('extra/selfie3.jpeg')
 with colbtt3:st.write(' ')
 
 
@@ -114,128 +115,127 @@ if image_file is not None:
   colbtt2:st.write(' ')
   file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8) #<class 'numpy.ndarray'>
   opencv_image = cv2.imdecode(file_bytes, 1) #<class 'numpy.ndarray'>
-else: 
-  opencv_image= cv2.imread('extra/selfie3.jpeg')
 
-[img_faces, num, boxes] = faceDetection(opencv_image)
-print("numero de rostros = "+ str(num))
-#st.write(boxes)
-#st.image(img_faces)
-if len(boxes) > 0:
-  display_app_header(main_txt = "🛠️ Step 2",
-                sub_txt= "Edit Image",
-                is_sidebar=True)
-  list = []
-  filename = 'saved_state.json'
 
-  for boxes in boxes:
-    list.append({
-      "type": "rect",
-        "left": boxes[0],
-        "top": boxes[1],
-        "width": boxes[2]-boxes[0],
-        "height": boxes[3]-boxes[1],
-        "fill": "#00FFB350",
-        "stroke": "#00FFB3",
-        "strokeWidth": 3
-    })
+  [img_faces, num, boxes] = faceDetection(opencv_image)
+  print("numero de rostros = "+ str(num))
+  #st.write(boxes)
+  #st.image(img_faces)
+  if len(boxes) > 0:
+    display_app_header(main_txt = "🛠️ Step 2",
+                  sub_txt= "Edit Image",
+                  is_sidebar=True)
+    list = []
+    filename = 'saved_state.json'
 
-  # Verify updated list
-  #st.write(list)
+    for boxes in boxes:
+      list.append({
+        "type": "rect",
+          "left": boxes[0],
+          "top": boxes[1],
+          "width": boxes[2]-boxes[0],
+          "height": boxes[3]-boxes[1],
+          "fill": "#00FFB350",
+          "stroke": "#00FFB3",
+          "strokeWidth": 3
+      })
 
-  listObj = {
-      "version": "4.4.0",
-      "objects": list  
-  }
+    # Verify updated list
+    #st.write(list)
 
-  # Verify updated listObj
-  #st.write(listObj)
+    listObj = {
+        "version": "4.4.0",
+        "objects": list  
+    }
 
-  with open(filename, 'w') as json_file:
-    json.dump(listObj, json_file, 
-                        indent=4,  
-                        separators=(',',': '))
+    # Verify updated listObj
+    #st.write(listObj)
 
-  with open(filename, "r") as f:   saved_state = json.load(f)
-  #st.write(saved_state)
-  
-  bg_image = Image.open(image_file)
-  label_color = (
-      st.sidebar.color_picker("Annotation color: ", "#97fdf5") 
-  )  # for alpha from 00 to FF
+    with open(filename, 'w') as json_file:
+      json.dump(listObj, json_file, 
+                          indent=4,  
+                          separators=(',',': '))
 
-  tool_mode = st.sidebar.selectbox(
-    "Select faces tool:", ("Add", "Move & edit")
-  )
-  mode = "transform" if tool_mode=="Move & edit" else "rect"
+    with open(filename, "r") as f:   saved_state = json.load(f)
+    #st.write(saved_state)
+    
+    bg_image = Image.open(image_file)
+    label_color = (
+        st.sidebar.color_picker("Annotation color: ", "#97fdf5") 
+    )  # for alpha from 00 to FF
 
-  canvas_result = st_canvas(
-      fill_color=label_color+ "50",
-      stroke_width=3,
-      stroke_color=label_color,
-      background_image=bg_image,
-      height=bg_image.height,
-      width=bg_image.width,
-      initial_drawing=saved_state,
-      drawing_mode=mode,
-      key="canvas_"+str(bg_image.height)+str(bg_image.width),
-  )
+    tool_mode = st.sidebar.selectbox(
+      "Select faces tool:", ("Add", "Move & edit")
+    )
+    mode = "transform" if tool_mode=="Move & edit" else "rect"
 
-  if canvas_result.json_data is not None:
-      print("Canvas creado")
-      rst_objects = canvas_result.json_data["objects"]
-      objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
-      n = int(len(rst_objects))
-      cols = st.columns(n)
-      st.info("🪄 IMAGE PROCESSED BY THE METHOD SUPER RESOLUTION (CNN)")
-      cols_srcnn = st.columns(n)
-      st.success("🪄 IMAGE PROCESSED BY THE METHOD SUPER RESOLUTION + ENHANCEMENT (GAN)")
-      cols_srgan = st.columns(n)
-      i = 0
+    canvas_result = st_canvas(
+        fill_color=label_color+ "50",
+        stroke_width=3,
+        stroke_color=label_color,
+        background_image=bg_image,
+        height=bg_image.height,
+        width=bg_image.width,
+        initial_drawing=saved_state,
+        drawing_mode=mode,
+        key="canvas_"+str(bg_image.height)+str(bg_image.width),
+    )
 
-      for rst_objects in rst_objects:
-        rts_boxes = [rst_objects['left'],rst_objects['top'],rst_objects['width']+rst_objects['left'],rst_objects['height']+rst_objects['top']]
-        #st.write(rts_boxes)
-        crop_image = crop_object(bg_image, rts_boxes)
-        cols[i].image(crop_image)
+    if canvas_result.json_data is not None:
+        print("Canvas creado")
+        rst_objects = canvas_result.json_data["objects"]
+        objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
+        n = int(len(rst_objects))
+        cols = st.columns(n)
+        st.info("🪄 IMAGE PROCESSED BY THE METHOD SUPER RESOLUTION (CNN)")
+        cols_srcnn = st.columns(n)
+        st.success("🪄 IMAGE PROCESSED BY THE METHOD SUPER RESOLUTION + ENHANCEMENT (GAN)")
+        cols_srgan = st.columns(n)
+        i = 0
 
-        #-------CNN-----
-        im_bgr = predictCNN(crop_image)
-        cols_srcnn[i].image(im_bgr)
+        for rst_objects in rst_objects:
+          rts_boxes = [rst_objects['left'],rst_objects['top'],rst_objects['width']+rst_objects['left'],rst_objects['height']+rst_objects['top']]
+          #st.write(rts_boxes)
+          crop_image = crop_object(bg_image, rts_boxes)
+          cols[i].image(crop_image)
 
-        im_rgb = im_bgr[:, :, [2, 1, 0]] #numpy.ndarray
-        ret, img_enco = cv2.imencode(".png", im_rgb)  #numpy.ndarray
-        srt_enco = img_enco.tobytes()   #bytes
-        img_BytesIO = BytesIO(srt_enco) #_io.BytesIO
-        img_BufferedReader = BufferedReader(img_BytesIO) #_io.BufferedReader
+          #-------CNN-----
+          im_bgr = predictCNN(crop_image)
+          cols_srcnn[i].image(im_bgr)
 
-        cols_srcnn[i].download_button(
-          label="📥",
-          data=img_BufferedReader,
-          file_name="srcnn_img_"+str(i)+".png",
-          mime="image/png"
-        )
+          im_rgb = im_bgr[:, :, [2, 1, 0]] #numpy.ndarray
+          ret, img_enco = cv2.imencode(".png", im_rgb)  #numpy.ndarray
+          srt_enco = img_enco.tobytes()   #bytes
+          img_BytesIO = BytesIO(srt_enco) #_io.BytesIO
+          img_BufferedReader = BufferedReader(img_BytesIO) #_io.BufferedReader
 
-        #cols_srgan[i].image(predictSrgan(crop_image))
-        #cols_srgan[i].image(predictSrgan("crop_img_0.png"))
-        img_gan=predictSrgan("crop_img_0.png")
-        cols_srgan[i].image(img_gan)
-        with open("results/restored_imgs/crop_img_0.png", "rb") as file:
-
-          cols_srgan[i].download_button(
-          label="📥",
-          data=file,
-          file_name="srgan_img_"+str(i)+".png",
-          mime="image/png"
+          cols_srcnn[i].download_button(
+            label="📥",
+            data=img_BufferedReader,
+            file_name="srcnn_img_"+str(i)+".png",
+            mime="image/png"
           )
-        print("img" + str(i))
-        i += 1
-      #for col in objects.select_dtypes(include=['object']).columns:
-      #    objects[col] = objects[col].astype("str")
-      #st.dataframe(objects)
 
-      #if st.button("Procesar"):
-      #  st.write("cargando")
-  
-else:
-  st.write("NO PERSON DETECTED")
+          #cols_srgan[i].image(predictSrgan(crop_image))
+          #cols_srgan[i].image(predictSrgan("crop_img_0.png"))
+          img_gan=predictSrgan("crop_img_0.png")
+          cols_srgan[i].image(img_gan)
+          with open("results/restored_imgs/crop_img_0.png", "rb") as file:
+
+            cols_srgan[i].download_button(
+            label="📥",
+            data=file,
+            file_name="srgan_img_"+str(i)+".png",
+            mime="image/png"
+            )
+          print("img" + str(i))
+          i += 1
+        #for col in objects.select_dtypes(include=['object']).columns:
+        #    objects[col] = objects[col].astype("str")
+        #st.dataframe(objects)
+
+        #if st.button("Procesar"):
+        #  st.write("cargando")
+    
+  else:
+    st.write("NO PERSON DETECTED")
