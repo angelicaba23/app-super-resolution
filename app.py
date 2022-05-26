ff4dc7c1bdfc9defdbc294435dcef537994c400b
Angelica Barranco <angelica.barranco@ibm.com>
2022-05-26 12:11:08 -0500

    extra added

--- app.py
+++ app.py
@@ -125,6 +125,7 @@ if image_file is not None or check:
     file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8) #<class 'numpy.ndarray'>
     opencv_image = cv2.imdecode(file_bytes, 1) #<class 'numpy.ndarray'>
     
+
   st.session_state.expandedval = False
   #check = False
   [img_faces, num, boxes] = faceDetection(opencv_image)
